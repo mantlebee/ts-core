83caa77d58036013fae1369853e4c2b3655adc13
mantlebee <mantlebee@outlook.com>
2022-06-03 11:44:26 +0200

    Correct bundle version

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,16 @@
-import path from "path";
-
-import alias from "@rollup/plugin-alias";
 import cleaner from "rollup-plugin-cleaner";
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 
 const outDir = "dist"
 
 export default {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: {
         dir: outDir,
-        format: "es",
+        format: "cjs",
     },
     plugins: [
         cleaner({ targets: [outDir] }),
-        alias({
-            resolve: [".ts"],
-            entries: { "@/": path.resolve(__dirname, "src/") }
-        }),
-        typescript()]
+        typescript()
+    ]
 };
