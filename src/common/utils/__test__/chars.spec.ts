6460e4db070afb8d7b4272c749c4fe66a979ca83
mantlebee <mantlebee@outlook.com>
2022-06-04 09:08:37 +0200

    test: chars

---
+++ src/common/utils/__test__/chars.spec.ts
@@ -0,0 +1,55 @@
+import {
+  getCharsFromCharCodesRange,
+  getLowerCaseChars,
+  getNumberChars,
+  getSpecialChars,
+  getUpperCaseChars,
+} from "../chars";
+
+const specialCharsGroups = [
+  // Char codes from 32 to 48
+  " !\"#$%&'()*+,-./",
+  // Char codes from 58 to 64
+  ":;<=>?@",
+  //Char codes from 94 to 96
+  "^_`",
+  //Char codes from 123 to 126
+  "{|}~",
+];
+
+describe("common", () => {
+  describe("utils", () => {
+    describe("chars", () => {
+      describe("getCharsFromCharCodesRange", () => {
+        it("String of chars converted from the provided char code range, ordered by char code", () => {
+          const str = getCharsFromCharCodesRange(32, 16);
+          expect(str).toBe(specialCharsGroups[0]);
+        });
+      });
+      describe("getLowerCaseChars", () => {
+        it("String of the lowercase alphabet, orderd alphabetically", () => {
+          const str = getLowerCaseChars();
+          expect(str).toBe("abcdefghijklmnopqrstuvwxyz");
+        });
+      });
+      describe("getNumberChars", () => {
+        it("String of numbers from 0 to 9", () => {
+          const str = getNumberChars();
+          expect(str).toBe("0123456789");
+        });
+      });
+      describe("getUpperCaseChars", () => {
+        it("String of the upper alphabet, orderd alphabetically", () => {
+          const str = getUpperCaseChars();
+          expect(str).toBe("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        });
+      });
+      describe("getSpecialChars", () => {
+        it("String of special chars, ordered by char code", () => {
+          const str = getSpecialChars();
+          expect(str).toBe(specialCharsGroups.join(""));
+        });
+      });
+    });
+  });
+});
--- src/common/utils/chars.ts
+++ src/common/utils/chars.ts
@@ -1,3 +1,9 @@
+/**
+ * Generates a string of chars converted from the provided char code range.
+ * @param startIndex Starting char code.
+ * @param length Count of char codes to use.
+ * @returns
+ */
 export function getCharsFromCharCodesRange(
   startIndex: number,
   length: number
@@ -8,23 +14,39 @@ export function getCharsFromCharCodesRange(
     .join("");
 }
 
+/**
+ * Creates a lowercase string of the alphabet.
+ * @returns a lowercase string of the alphabet.
+ */
 export function getLowerCaseChars(): string {
   return getCharsFromCharCodesRange(97, 26);
 }
 
+/**
+ * Creates a string of the numbers from 0 to 9.
+ * @returns a string of the numbers from 0 to 9.
+ */
 export function getNumberChars(): string {
   return getCharsFromCharCodesRange(48, 10);
 }
 
+/**
+ * Creates a uppercase string of the alphabet.
+ * @returns a uppercase string of the alphabet.
+ */
 export function getUpperCaseChars(): string {
   return getCharsFromCharCodesRange(65, 26);
 }
 
+/**
+ * Creates a string with all the special chars.
+ * @returns a string with all the special chars.
+ */
 export function getSpecialChars(): string {
   return (
-    getCharsFromCharCodesRange(123, 4) +
-    getCharsFromCharCodesRange(94, 3) +
+    getCharsFromCharCodesRange(32, 16) +
     getCharsFromCharCodesRange(58, 7) +
-    getCharsFromCharCodesRange(32, 16)
+    getCharsFromCharCodesRange(94, 3) +
+    getCharsFromCharCodesRange(123, 4)
   );
 }
