2fb19cbba7eb357e6a01c02780fb178bd862b4da
mantlebee <mantlebee@outlook.com>
2022-06-04 17:03:12 +0200

    random float numbers, is integer

--- src/common/utils/__test__/numbers.spec.ts
+++ src/common/utils/__test__/numbers.spec.ts
@@ -1,18 +1,47 @@
 import { List } from "@/common";
 
-import { generateRandomNumber } from "../numbers";
+import { generateRandomNumber, numberIsInteger } from "../numbers";
 
 describe("common", () => {
   describe("utils", () => {
     describe("number", () => {
       describe("generateRandomNumber", () => {
-        it("Generates a random number in the given range, min and max included", () => {
-          const max = 9;
-          const min = 5;
+        it("Generates an integer random number between 0 and the given max value, max included", () => {
+          const max = 12;
           const randoms: List<number> = [];
-          for (let i = 0; i < 10; ++i)
-            randoms.push(generateRandomNumber(min, max));
+          for (let i = 0; i < 99; ++i) randoms.push(generateRandomNumber(max));
+          expect(randoms.every((a) => a <= max)).toBeTruthy();
+          expect(randoms.every(numberIsInteger)).toBeTruthy();
+        });
+        it("Generates an integer random number in the given range, min and max included", () => {
+          const max = 12;
+          const min = 10;
+          const randoms: List<number> = [];
+          for (let i = 0; i < 99; ++i)
+            randoms.push(generateRandomNumber(max, min));
           expect(randoms.every((a) => a >= min && a <= max)).toBeTruthy();
+          expect(randoms.every(numberIsInteger)).toBeTruthy();
+        });
+        it("Generates a float (2 digits) random number in the given range, min and max included", () => {
+          const decimals = 2;
+          const max = 12;
+          const min = 10;
+          const randoms: List<number> = [];
+          for (let i = 0; i < 99; ++i)
+            randoms.push(generateRandomNumber(max, min, decimals));
+          expect(randoms.every((a) => a >= min && a <= max)).toBeTruthy();
+          expect(randoms.some((a) => `${a}`.length === 5)).toBeTruthy();
+          expect(randoms.some((a) => !numberIsInteger(a))).toBeTruthy();
+        });
+      });
+      describe("numberIsInteger", () => {
+        it("Number is integer", () => {
+          const isInteger = numberIsInteger(5);
+          expect(isInteger).toBeTruthy();
+        });
+        it("Number is not integer", () => {
+          const isInteger = numberIsInteger(5.12);
+          expect(isInteger).toBeFalsy();
         });
       });
     });
--- src/common/utils/numbers.ts
+++ src/common/utils/numbers.ts
@@ -1,9 +1,31 @@
 /**
- * Generates a random integer number between the given min and max values.
- * @param min Minimum number that can be generated.
+ * Generates a random integer/float number between the given min and max values, max included.
+ * To return an integer leave `decimals` to 0.
+ * If `decimals` is greater than 0, the max value will be an integer anyway.
  * @param max Maximum number that can be generated.
- * @returns a random number between min and max.
+ * @param min Minimum number that can be generated.
+ * @param decimals Digits after the decimals point.
+ * @returns a random number between min and max, max included.
  */
-export function generateRandomNumber(min = 0, max = 1): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+export function generateRandomNumber(
+  max: number,
+  min = 0,
+  decimals = 0
+): number {
+  const random = Math.random() * (max - min + 1) + min;
+  if (decimals) {
+    if (random >= max) return max;
+    return parseFloat(random.toFixed(decimals));
+  } else {
+    return Math.floor(random);
+  }
+}
+
+/**
+ * Checks if a number is integer or not.
+ * @param numb Number to check.
+ * @returns true is number is integer.
+ */
+export function numberIsInteger(numb: number): boolean {
+  return numb % 1 == 0;
 }
