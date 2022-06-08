9751a22235d20ed45e9decb0bf0a9331274c89ce
mantlebee <mantlebee@outlook.com>
2022-06-08 21:51:27 +0200

    Missing documentation @param

--- src/common/types/objects.ts
+++ src/common/types/objects.ts
@@ -16,6 +16,7 @@ export type KeyOf<TObject, TKey = string> = Extract<keyof TObject, TKey>;
 /**
  * Keys of an object. Value is the same of the original object or different, if specified.
  * @param TObject Object from which to extract the keys.
+ * @param TValue Type of the values of the dictionary. The default is the same of the object.
  */
 export type KeysOf<TObject, TValue = TObject[keyof TObject]> = {
   [TKey in keyof TObject]: TValue;
@@ -24,6 +25,7 @@ export type KeysOf<TObject, TValue = TObject[keyof TObject]> = {
 /**
  * Keys of an object; all keys are optional. Value is the same of the original object or different, if specified.
  * @param TObject Object from which to extract the keys.
+ * @param TValue Type of the values of the dictionary. The default is the same of the object.
  */
 export type OptionalKeysOf<TObject, TValue = TObject[keyof TObject]> = {
   [TKey in keyof TObject]?: TValue;
