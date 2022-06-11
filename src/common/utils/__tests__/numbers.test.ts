f2713386eebba8ad63634fab34780629e20386c0
mantlebee <mantlebee@outlook.com>
2022-06-11 10:36:31 +0200

    roundNumber

--- src/common/utils/__tests__/numbers.test.ts
+++ src/common/utils/__tests__/numbers.test.ts
@@ -1,4 +1,4 @@
-import { isNumber, numberIsInteger } from "../numbers";
+import { isNumber, numberIsInteger, roundNumber } from "../numbers";
 
 describe("common", () => {
   describe("utils", () => {
@@ -21,6 +21,32 @@ describe("common", () => {
         it("Number is not integer", () => {
           const isInteger = numberIsInteger(5.12);
           expect(isInteger).toBeFalsy();
+        });
+      });
+      describe("roundNumber", () => {
+        it("Rounds 1.4 to 1", () => {
+          const result = roundNumber(1.4);
+          expect(result).toBe(1);
+        });
+        it("Rounds 1.44 to 1.4", () => {
+          const result = roundNumber(1.44, 1);
+          expect(result).toBe(1.4);
+        });
+        it("Rounds 1.444 to 1.44", () => {
+          const result = roundNumber(1.444, 2);
+          expect(result).toBe(1.44);
+        });
+        it("Rounds 1.5 to 2", () => {
+          const result = roundNumber(1.5);
+          expect(result).toBe(2);
+        });
+        it("Rounds 1.55 to 1.6", () => {
+          const result = roundNumber(1.55, 1);
+          expect(result).toBe(1.6);
+        });
+        it("Rounds 1.555 to 1.56", () => {
+          const result = roundNumber(1.555, 2);
+          expect(result).toBe(1.56);
         });
       });
     });
--- src/common/utils/numbers.ts
+++ src/common/utils/numbers.ts
@@ -17,3 +17,16 @@ export function isNumber(arg: Any): boolean {
 export function numberIsInteger(numb: number): boolean {
   return numb % 1 == 0;
 }
+
+/**
+ * Rouns a number, keeping a number of decimals equal to the decimals parameter.
+ * Reference: https://medium.com/swlh/how-to-round-to-a-certain-number-of-decimal-places-in-javascript-ed74c471c1b8
+ * @param numb Number to round.
+ * @param decimals Number of decimals to keep.
+ * @returns a rounded number with N decimals.
+ */
+export function roundNumber(numb: number, decimals = 0): number {
+  return Number(
+    Math.round(parseFloat(`${numb}e${decimals}`)) + "e-" + decimals
+  );
+}
