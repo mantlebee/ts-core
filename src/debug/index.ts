7a9fd3592c1f91cd9f6d23e324686adb9c7f7ec6
mantlebee <mantlebee@outlook.com>
2022-06-24 00:27:11 +0200

    Doc for isDebug

--- src/debug/index.ts
+++ src/debug/index.ts
@@ -1,4 +1,10 @@
 let debug = false;
+
+/**
+ * Gets or sets the debug value. If value is defined, its value is used to set/unset debug mode.
+ * @param value Debug value to set.
+ * @returns current debug value.
+ */
 export function isDebug(value?: boolean): boolean {
   if (value !== undefined) debug = value;
   return debug;
