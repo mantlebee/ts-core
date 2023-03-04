92d9e00657c2be797bf02eea686d9a214f37b857
mantlebee <mantlebee@outlook.com>
2023-03-04 07:19:22 +0100

    Used Any type instead any

--- src/loggers/models.ts
+++ src/loggers/models.ts
@@ -1,30 +1,30 @@
+import { Any } from "@/common";
+
 import { LoggerTypes } from "./contants";
 import { ILogger } from "./interfaces";
 import { LogDelegate } from "./types";
 
 export class Logger implements ILogger {
-  private logDelegate: LogDelegate;
-
-  public constructor(logDelegate: LogDelegate) {
+  public constructor(private readonly logDelegate: LogDelegate) {
     this.logDelegate = logDelegate;
   }
 
-  public log(type: LoggerTypes, message: string, data?: any): void {
+  public log(type: LoggerTypes, message: string, data?: Any): void {
     this.logDelegate(type, message, data);
   }
-  public logDebug(message: string, data?: any): void {
+  public logDebug(message: string, data?: Any): void {
     this.log(LoggerTypes.debug, message, data);
   }
-  public logError(message: string, data?: any): void {
+  public logError(message: string, data?: Any): void {
     this.log(LoggerTypes.error, message, data);
   }
-  public logInfo(message: string, data?: any): void {
+  public logInfo(message: string, data?: Any): void {
     this.log(LoggerTypes.info, message, data);
   }
-  public logSuccess(message: string, data?: any): void {
+  public logSuccess(message: string, data?: Any): void {
     this.log(LoggerTypes.success, message, data);
   }
-  public logWarning(message: string, data?: any): void {
+  public logWarning(message: string, data?: Any): void {
     this.log(LoggerTypes.warning, message, data);
   }
 }
--- src/loggers/subscribable-logger/models.ts
+++ src/loggers/subscribable-logger/models.ts
@@ -1,3 +1,4 @@
+import { Any } from "@/common";
 import { ISubscribable, Subscribable, Subscription } from "@/subscriptions";
 
 import { LoggerTypes } from "../contants";
@@ -5,16 +6,19 @@ import { ILogger } from "../interfaces";
 import { Logger } from "../models";
 import { SubscribableLoggerData } from "./types";
 
-export class SubscribableLogger extends Logger implements ILogger, ISubscribable<SubscribableLoggerData> {
-    private _subscribable = new Subscribable<SubscribableLoggerData>()
+export class SubscribableLogger
+  extends Logger
+  implements ILogger, ISubscribable<SubscribableLoggerData>
+{
+  private _subscribable = new Subscribable<SubscribableLoggerData>();
 
-    public override log(type: LoggerTypes, message: string, data?: any): void {
-        this._subscribable.notifyData({type, message, data })
-    }
-    public subscribe(subscription: Subscription<SubscribableLoggerData>): number {
-        return this._subscribable.subscribe(subscription)
-    }
-    public unsubscribe(id: number): void {
-        this._subscribable.unsubscribe(id)
-    }
+  public override log(type: LoggerTypes, message: string, data?: Any): void {
+    this._subscribable.notifyData({ type, message, data });
+  }
+  public subscribe(subscription: Subscription<SubscribableLoggerData>): number {
+    return this._subscribable.subscribe(subscription);
+  }
+  public unsubscribe(id: number): void {
+    this._subscribable.unsubscribe(id);
+  }
 }
