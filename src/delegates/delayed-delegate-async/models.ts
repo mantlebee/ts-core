a84fe10e6a94f66bae4bb307e35a67a95cca0dee
mantlebee <mantlebee@outlook.com>
2023-10-10 16:02:49 +0200

    Removed reference to Delegate

--- src/delegates/delayed-delegate-async/models.ts
+++ src/delegates/delayed-delegate-async/models.ts
@@ -1,4 +1,4 @@
-import { Delegate, Nullable } from "@/common";
+import { Nullable } from "@/common";
 
 import { IDelegate } from "../interfaces";
 
@@ -7,11 +7,12 @@ import { IDelegate } from "../interfaces";
  * Delays the execution of an async {@link delegate}, if the {@link call} method is called multiple times in a time range ({@link delay}).
  */
 export class DelayedDelegateAsync<TResult = void>
-  implements IDelegate<Promise<TResult>> {
+  implements IDelegate<Promise<TResult>>
+{
   /** Milliseconds after which the {@link delegate} must be executed. */
   private readonly delay!: number;
   /** Async function to execute. */
-  private readonly delegate!: Delegate<Promise<TResult>>;
+  private readonly delegate!: () => Promise<TResult>;
   /**
    * Timeout that is set and reset every time the {@link call} method is called.
    * After its expiring, the {@link delegate} is executed.
@@ -21,7 +22,7 @@ export class DelayedDelegateAsync<TResult = void>
    * @param delegate {@link delegate}
    * @param delay {@link delay}
    */
-  public constructor(delegate: Delegate<Promise<TResult>>, delay: number) {
+  public constructor(delegate: () => Promise<TResult>, delay: number) {
     this.delay = delay;
     this.delegate = delegate;
   }
