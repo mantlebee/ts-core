1b7f1823313f33889760e9f1cf779b8c5b0278ae
mantlebee <mantlebee@outlook.com>
2023-10-10 14:48:58 +0200

    Removed Delegate type

--- src/common/types/functions.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Type of function without args or with a single one that returns a value or not.
- * It is an alternative to arrow function declarations, like `() => void`, `() => string`, `(a: string) => boolean`.
- * @param TReturn Result of the function.
- * @param TArg Single and optional function parameter.
- */
-export type Delegate<TReturn = void, TArg = void> = (arg?: TArg) => TReturn;
