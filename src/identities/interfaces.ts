0911955817295d72abdcf253baec125971d7a652
mantlebee <mantlebee@outlook.com>
2023-10-10 18:20:09 +0200

    Fixed/extended documentation for identities

--- src/identities/interfaces.ts
+++ src/identities/interfaces.ts
@@ -1,5 +1,8 @@
 import { Nullable } from "@/common";
 
+/**
+ * Represents an identity manager which has to generate new ids and track the last one generated.
+ */
 export interface IIdentityManager<T> {
   readonly lastValue: Nullable<T>;
   newValue(): T;
