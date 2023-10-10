d2153a1f3941064d8fa923f17a82bc067674e20e
mantlebee <mantlebee@outlook.com>
2023-10-10 18:33:36 +0200

    Refactoring of Logger

--- src/loggers/models.ts
+++ src/loggers/models.ts
@@ -1,30 +1,32 @@
 import { Any } from "@/common";
 
-import { LoggerTypes } from "./contants";
+import { LogTypes } from "./contants";
 import { ILogger } from "./interfaces";
 import { LogDelegate } from "./types";
 
 export class Logger implements ILogger {
-  public constructor(private readonly logDelegate: LogDelegate) {
-    this.logDelegate = logDelegate;
+  private readonly _logDelegate: LogDelegate;
+
+  public constructor(logDelegate: LogDelegate) {
+    this._logDelegate = logDelegate;
   }
 
-  public log(type: LoggerTypes, message: string, data?: Any): void {
-    this.logDelegate(type, message, data);
+  public log(type: LogTypes, message: string, data?: Any): void {
+    this._logDelegate(type, message, data);
   }
   public logDebug(message: string, data?: Any): void {
-    this.log(LoggerTypes.debug, message, data);
+    this.log(LogTypes.debug, message, data);
   }
   public logError(message: string, data?: Any): void {
-    this.log(LoggerTypes.error, message, data);
+    this.log(LogTypes.error, message, data);
   }
   public logInfo(message: string, data?: Any): void {
-    this.log(LoggerTypes.info, message, data);
+    this.log(LogTypes.info, message, data);
   }
   public logSuccess(message: string, data?: Any): void {
-    this.log(LoggerTypes.success, message, data);
+    this.log(LogTypes.success, message, data);
   }
   public logWarning(message: string, data?: Any): void {
-    this.log(LoggerTypes.warning, message, data);
+    this.log(LogTypes.warning, message, data);
   }
 }
