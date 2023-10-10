225df367d6b047a051aa49628ba3f91147fe4d4c
mantlebee <mantlebee@outlook.com>
2023-10-10 18:37:31 +0200

    Fixed/extended documentation of subscriptions

--- src/subscriptions/subscribable/interfaces.ts
+++ src/subscriptions/subscribable/interfaces.ts
@@ -1,5 +1,9 @@
 import { Subscription } from "./types";
 
+/**
+ * Represents a subscribable element. It allows to (un)subscribe to an instance.
+ * When the subscribers are notified, depends on the implementation of the interface.
+ */
 export interface ISubscribable<TData, TId = number> {
   subscribe(subscription: Subscription<TData>): TId;
   unsubscribe(id: TId): void;
--- src/subscriptions/subscribable/models.ts
+++ src/subscriptions/subscribable/models.ts
@@ -4,10 +4,17 @@ import { NumericIdentityManager } from "@/identities";
 import { ISubscribable } from "./interfaces";
 import { Subscription } from "./types";
 
+/**
+ * Implementation of {@link ISubscribable} with a {@link notifyData} to call the subscribers.
+ */
 export class Subscribable<TData> implements ISubscribable<TData> {
   private identityManager = new NumericIdentityManager();
   private subscriptions: Dictionary<Subscription<TData>> = {};
 
+  /**
+   * Calls all subscribers.
+   * @param data Data to pass to subscribers.
+   */
   public notifyData(data: TData): void {
     Object.values(this.subscriptions).forEach((a) => a(data));
   }
